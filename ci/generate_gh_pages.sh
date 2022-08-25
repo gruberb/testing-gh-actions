63ef48190a275dc6be7833cb22577a8febc56e45
Bastian Gruber <foreach@me.com>
2022-08-25 10:52:22 +0200

    Maybe?

--- ci/generate_gh_pages.sh
+++ ci/generate_gh_pages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+cargo doc --all --no-deps
 rm -rf ./docs
 echo "<meta http-equiv=\"refresh\" content=\"0; url=build_wheel\\/index.html\">" > target/doc/index.html
 cp -r target/doc ./docs
