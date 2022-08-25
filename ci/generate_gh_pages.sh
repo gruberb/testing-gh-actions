1d8aae8bfc42d6466e4e28def49d61b8061d8298
Bastian Gruber <foreach@me.com>
2022-08-25 12:02:33 +0200

    Maybe now?

--- ci/generate_gh_pages.sh
+++ ci/generate_gh_pages.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-rm -rf docs
-mkdir docs
-echo "<meta http-equiv=\"refresh\" content=\"0; url=testing_gh_actions/index.html\">" > ./docs/index.html
-cp -r ./target/doc/* ./docs
+echo "<meta http-equiv=\"refresh\" content=\"0; url=testing_gh_actions/index.html\">" > ./target/doc/index.html
